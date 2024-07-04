8040eb078fbce284b69c7b0f1a9fde7f69ad15c7
PhoebeIV <a.fletcher1991@live.com>
2024-07-04 12:36:06 +1000

    Update GrantConditionWhileProducing.cs
    (still doesn't work)

--- OpenRA.Mods.Common/Traits/Conditions/GrantConditionWhileProducing.cs
+++ OpenRA.Mods.Common/Traits/Conditions/GrantConditionWhileProducing.cs
@@ -16,11 +16,14 @@ using OpenRA.Traits;
 
 namespace OpenRA.Mods.OpenSA.Traits.Conditions
 {
-	public class GrantConditionWhileProducingInfo : TraitInfo, Requires<ProductionQueueInfo>
+	public class GrantConditionWhileProducingInfo : PausableConditionalTraitInfo, Requires<ProductionInfo>
 	{
 		[FieldLoader.Require]
 		[GrantedConditionReference]
 		public readonly string Condition = null;
+
+		[Desc("Queues that should be producing for this overlay to render.")]
+		public readonly HashSet<string> Queues = new();
 
 		public override object Create(ActorInitializer init)
 		{
@@ -28,28 +31,59 @@ namespace OpenRA.Mods.OpenSA.Traits.Conditions
 		}
 	}
 
-	public class GrantConditionWhileProducing : ITick
+	public class GrantConditionWhileProducing : PausableConditionalTrait<GrantConditionWhileProducingInfo>, INotifyOwnerChanged
 	{
+		readonly Actor self;
 		readonly GrantConditionWhileProducingInfo info;
-		readonly IEnumerable<ProductionQueue> productionQueues;
-
+		readonly ProductionInfo[] productionQueues;
+		ProductionQueue[] queues;
 		int token = Actor.InvalidConditionToken;
-
-		public GrantConditionWhileProducing(Actor self, GrantConditionWhileProducingInfo info)
+		bool IsProducing
 		{
-			this.info = info;
-			productionQueues = self.TraitsImplementing<ProductionQueue>();
+			get { return queues != null && queues.Any(q => q.Enabled && q.AllQueued().Any(i => !i.Paused && i.Started) && q.MostLikelyProducer().Actor == self); }
 		}
 
-		void ITick.Tick(Actor self)
+		public GrantConditionWhileProducing(Actor self, GrantConditionWhileProducingInfo info)
+			: base(info)
 		{
 			var hasCondition = token != Actor.InvalidConditionToken;
-			var isProducing = productionQueues.Any(queue => queue.AllQueued().Any());
 
-			if (!hasCondition && isProducing)
+			this.info = info;
+			productionQueues = self.Info.TraitInfos<ProductionInfo>().ToArray();
+
+			if (!hasCondition && IsProducing)
 				token = self.GrantCondition(info.Condition);
-			else if (hasCondition && !isProducing)
+			else if (hasCondition && !IsProducing)
 				token = self.RevokeCondition(token);
+		}
+
+		void CacheQueues(Actor self)
+		{
+			// Per-actor production
+			queues = self.TraitsImplementing<ProductionQueue>()
+				.Where(q =>
+					productionQueues.Any(p => p.Produces.Contains(q.Info.Type)) &&
+					(Info.Queues.Count == 0 || Info.Queues.Contains(q.Info.Type)))
+				.ToArray();
+
+			if (queues.Length == 0)
+			{
+				// Player-wide production
+				queues = self.Owner.PlayerActor.TraitsImplementing<ProductionQueue>()
+					.Where(q =>
+						productionQueues.Any(p => p.Produces.Contains(q.Info.Type)) &&
+						(Info.Queues.Count == 0 || Info.Queues.Contains(q.Info.Type)))
+					.ToArray();
+			}
+		}
+
+		protected override void TraitEnabled(Actor self)
+		{
+			CacheQueues(self);
+		}
+		void INotifyOwnerChanged.OnOwnerChanged(Actor self, Player oldOwner, Player newOwner)
+		{
+			self.World.AddFrameEndTask(w => CacheQueues(self));
 		}
 	}
 }
